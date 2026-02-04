d5d059d5bee2e8cf130856b9f510ec95662d03b4
tao.jun <61566027@163.com>
2026-02-04 13:13:24 +0800

    1

--- myself/proxy_v2ray/setup.sh
+++ myself/proxy_v2ray/setup.sh
@@ -18,6 +18,45 @@ cat >> "$BASHRC" << BLOCK_END
 alias setproxy='source $PROXY_ENV set http://39.155.179.4:20171'
 # 取消代理
 alias unsetproxy='source $PROXY_ENV unset'
+# 设置 git 代理 (HTTP)
+alias gitsetproxy='git config --global http.proxy http://39.155.179.4:20171 && git config --global https.proxy http://39.155.179.4:20171'
+
+# 取消 git 代理
+alias gitunsetproxy='git config --global --unset http.proxy && git config --global --unset https.proxy'
+
+# 设置 git SSH 代理 (通过 SOCKS5)
+gitsshsetproxy() {
+  mkdir -p ~/.ssh
+  if ! grep -q "# GIT-SSH-PROXY-START" ~/.ssh/config 2>/dev/null; then
+    cat >> ~/.ssh/config << 'SSHEOF'
+
+# GIT-SSH-PROXY-START
+Host github.com
+    HostName github.com
+    User git
+    ProxyCommand nc -X 5 -x 39.155.179.4:20170 %h %p
+
+Host gitlab.com
+    HostName gitlab.com
+    User git
+    ProxyCommand nc -X 5 -x 39.155.179.4:20170 %h %p
+# GIT-SSH-PROXY-END
+SSHEOF
+    echo "已添加 SSH 代理配置到 ~/.ssh/config"
+  else
+    echo "SSH 代理配置已存在"
+  fi
+}
+
+# 取消 git SSH 代理
+gitsshunsetproxy() {
+  if [ -f ~/.ssh/config ]; then
+    sed -i '/# GIT-SSH-PROXY-START/,/# GIT-SSH-PROXY-END/d' ~/.ssh/config
+    echo "已移除 SSH 代理配置"
+  else
+    echo "~/.ssh/config 不存在"
+  fi
+}
 BLOCK_END
 
 echo "已添加。执行 source $BASHRC 或重新登录后生效。"
